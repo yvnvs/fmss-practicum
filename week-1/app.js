7b5a8225d8c3f9fcb47cd834b7179e5f572d56cc
Yunus <yunusinne@gmail.com>
2023-04-01 19:57:53 +0300

    refactor: added

--- week-1/app.js
+++ week-1/app.js
@@ -1,18 +1,21 @@
-import axios from "axios";
+import axios from "axios"; //axios imported
 
 async function getData(id) {
   try {
+    // Request is sent to retrieve user data
     const userResponse = await axios.get(
       `https://jsonplaceholder.typicode.com/users/${id}`
     );
     const userData = userResponse.data;
 
+    // Request is sent to retrieve post data
     const postResponse = await axios.get(
       `https://jsonplaceholder.typicode.com/posts?id=${id}`
     );
     const postData = postResponse.data;
 
     let data = Object.assign(userData, postData);
+
     console.log(data);
   } catch (error) {
     console.log(error.message);
