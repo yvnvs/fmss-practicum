44ab88def678ed56394ec6e287d58a72d33216c5
Yunus <yunusinne@gmail.com>
2023-04-17 02:56:58 +0300

    EmojiResultRow test

--- week-3/emoji-search/src/EmojiResultRow.js
+++ week-3/emoji-search/src/EmojiResultRow.js
@@ -5,7 +5,7 @@ import "./EmojiResultRow.css";
 export default class EmojiResultsRow extends PureComponent {
   static propTypes = {
     title: PropTypes.string,
-    symbol: PropTypes.string
+    symbol: PropTypes.string,
   };
 
   render() {
@@ -14,6 +14,7 @@ export default class EmojiResultsRow extends PureComponent {
     return (
       <div
         className="component-emoji-result-row copy-to-clipboard"
+        data-testid="emoji-row-test"
         data-clipboard-text={this.props.symbol}
       >
         <img alt={this.props.title} src={src} />
