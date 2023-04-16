4fc6b35f0aac05881365904cdfcbb4ef3e29d0f0
Yunus <yunusinne@gmail.com>
2023-04-17 02:28:24 +0300

    header test

---
+++ week-3/emoji-search/src/test/Header.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Header from "../Header";
+
+test("Header render", () => {
+  render(<Header />);
+
+  const headerDom = screen.getByText(/Emoji Search/i);
+  expect(headerDom).toBeInTheDocument();
+});
